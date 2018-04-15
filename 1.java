1c897cb63b8f8879fa890454e630238b3f8fadf6
jasyip <jasonyip1234567@gmail.com>
2018-04-15 05:02:26 -0700

    Delete 1.java

--- 1.java
+++
@@ -1,8 +0,0 @@
-public class name {
-	public static void main(String[] args) {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		System.out.println("You're not even booraj");
-	}
-}
