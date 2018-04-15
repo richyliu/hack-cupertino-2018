e173f8c45a6710ccc79aba2b6777ae747b625f87
Abhinav Kommula <akommula@gmail.com>
2018-04-14 18:21:41 -0700

    Update 1.java

--- 1.java
+++ 1.java
@@ -2,5 +2,7 @@ public class name {
 	public static void main(String[] args) {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		System.out.println("You're not even booraj");
 	}
 }
