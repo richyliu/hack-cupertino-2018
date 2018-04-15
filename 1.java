a7790bc0416d5e9fcdace161dcca25e95c3c274c
jasyip <jasonyip1234567@gmail.com>
2018-04-14 18:21:00 -0700

    preparing
    stringtokenizer also included

---
+++ 1.java
@@ -0,0 +1,6 @@
+public class name {
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+	}
+}
